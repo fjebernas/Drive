e54abf0be1887dab3d5278f481d012d9274c6f42
francisjosephironman <francis.3.6.joseph@gmail.com>
2023-02-09 02:49:57 +0800

    Updated search/filter interactivity

--- drive-frontend/src/components/FilterableList/ProverbsList.jsx
+++ drive-frontend/src/components/FilterableList/ProverbsList.jsx
@@ -1,12 +1,17 @@
 import ProverbRow from "./ProverbRow";
 
 function ProverbsList(props) {
+
+  const filteredProverbs = props.proverbs.reduce((result, proverb) => {
+    proverb.content.indexOf(props.filterText) > -1 && result.push(proverb);
+    return result;
+  }, []);
+
   return (
     <ul className="list-group list-group-flush">
       {
-        props.proverbs.length > 0 ?
-        props.proverbs
-        .filter(proverb => proverb.content.indexOf(props.filterText) === -1 ? false : true)
+        filteredProverbs.length > 0 ?
+        filteredProverbs
         .map(proverb => (
           <li key={proverb.id} className="list-group-item">
             <ProverbRow id={proverb.id} content={proverb.content} country={proverb.country} />
